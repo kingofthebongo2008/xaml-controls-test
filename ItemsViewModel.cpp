40115e82938dedfe1eb70717d1d43b75be0959d4
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2019-12-16 07:02:20 +0200

    added view mode

---
+++ ItemsViewModel.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "ItemsViewModel.h"
+#include "ItemsViewModel.g.cpp"
+#include "Item.g.cpp"
+
+namespace winrt::App1::implementation
+{
+    using namespace winrt;
+    using namespace Microsoft::UI::Xaml;
+
+    ItemsViewModel::ItemsViewModel()
+    {
+        
+    }
+
+    Item::Item()
+    {
+
+    }
+}
---
+++ ItemsViewModel.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "ItemsViewModel.g.h"
+#include "Item.g.h"
+
+namespace winrt::App1::implementation
+{
+    struct ItemsViewModel : ItemsViewModelT<ItemsViewModel>
+    {
+        ItemsViewModel();
+     
+    };
+
+    struct Item : ItemT<Item>
+    {
+        Item();
+
+        hstring Name()   const    { return to_hstring(L"Name"); }
+        hstring Position() const  { return to_hstring(L"Position"); }
+
+
+        void Position(hstring v) {}
+        void Name(hstring v) {}
+
+
+
+    };
+}
+
+namespace winrt::App1::factory_implementation
+{
+    struct ItemsViewModel : ItemsViewModelT<ItemsViewModel, implementation::ItemsViewModel>
+    {
+    };
+
+    struct Item : ItemT<Item, implementation::Item>
+    {
+    };
+}
